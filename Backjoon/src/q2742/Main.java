6bb41c17ce7c5b60c74b7093568cc4c761a6cb1c
33cks <33cks@LAPTOP-QRDLLPVD>
2024-12-30 14:34:31 +0900

    2742

---
+++ Backjoon/src/q2742/Main.java
@@ -0,0 +1,15 @@
+package q2742;
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        int n;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        
+        for (int i = n; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
