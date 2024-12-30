8189f764f05cc79773ba27bff9bd80aec980eec4
33cks <33cks@LAPTOP-QRDLLPVD>
2024-12-30 14:15:49 +0900

    Scanner(System.in);

--- Backjoon/src/q2741/Main.java
+++ Backjoon/src/q2741/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		int n;
-		Scanner scan = new Scanner (System.in);
+		Scanner scan = new Scanner(System.in);
 		n = scan.nextInt();
 		
 		for (int i = 1; i <= n; i++) {
