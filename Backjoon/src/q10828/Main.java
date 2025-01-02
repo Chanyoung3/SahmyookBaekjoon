a5400b76300a7527a7157faa2e7f5b502342ad88
User <User@DESKTOP-0A2DQHL>
2025-01-02 23:47:16 +0900

    Initial Commit

--- Backjoon/src/q10828/Main.java
+++ Backjoon/src/q10828/Main.java
@@ -6,30 +6,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int icom = sc.nextInt();
-		String push, pop, size, empty;
+		int icom = sc.nextInt();;
 		int top = -1;
 		int[] stack = new int[icom];
-		for(int i = 0; i < icom; i++) {
-			if(sc.next().equals("push")) {
-				stack[++top] = sc.nextInt();
-				System.out.println(stack[top]);
-			}
-			else if(sc.next().equals("pop")) {
-				System.out.println(stack[top]);
-				stack[top--] = 0;
-			}
-			else if(sc.next().equals("size")) {
-				
-			}
-			else if(sc.next().equals("empty")) {
-				if(top == -1) System.out.println("1");
-				else System.out.println("0");
-			}
-			else if(sc.next().equals("top")) {
-				
-			}
-		}
+		
+		for (int i = 0; i < icom; i++) {
+            String command = sc.next(); // 명령어 입력
+
+            if (command.equals("push")) {
+                int value = sc.nextInt(); // 추가할 값 입력
+                stack[++top] = value; // top 증가 후 값 추가
+            } else if (command.equals("pop")) {
+                if (top == -1) {
+                    System.out.println("-1"); // 스택이 비었을 경우
+                } else {
+                    System.out.println(stack[top]); // top 값 출력
+                    top--; // top 감소
+                }
+            } else if (command.equals("size")) {
+                System.out.println(top + 1); // 스택 크기 출력
+            } else if (command.equals("empty")) {
+                if (top == -1) {
+                    System.out.println("1"); // 스택이 비어 있음
+                } else {
+                    System.out.println("0"); // 스택이 비어 있지 않음
+                }
+            } else if (command.equals("top")) {
+                if (top == -1) {
+                    System.out.println("-1"); // 스택이 비었을 경우
+                } else {
+                    System.out.println(stack[top]); // top 값 출력
+                }
+            }
+        }
 		
 		
 		sc.close();
