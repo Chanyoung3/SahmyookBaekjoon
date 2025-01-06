2aa2de5082f8a8b6bb6b5cc1732f0791101b74a1
33cks <33cks@LAPTOP-QRDLLPVD>
2025-01-06 14:15:02 +0900

    Initial Commit

---
+++ Backjoon/src/q3052/Main.java
@@ -0,0 +1,37 @@
+package q3052;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		int[] rem = new int[10];
+		int cnt = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		for(int i = 0; i < rem.length; i++) {
+			rem[i] = arr[i] % 42;
+		}
+		
+		for(int i = 0; i < rem.length; i++) { 
+			for(int j = 0; j < rem.length; j++) {
+				if(i <= j) continue;
+				
+				if(rem[i] == rem[j]) rem[j] = -1;
+			} 
+		}
+		
+		for(int i = 0; i < rem.length; i++) {
+			if(rem[i] != -1) cnt++;
+		}
+		
+		System.out.println(cnt);
+	}
+}
--- Backjoon/src/q8958/Main.java
+++ Backjoon/src/q8958/Main.java
@@ -8,10 +8,24 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int qz = sc.nextInt();
 		
-		for(int i = 0; i < qz; i++) {
+		for(int i = 0; i <= qz; i++) {
 			String res = sc.nextLine();
-			String[] arr = res.split("");	
+			String[] arr = res.split("");
+			if(arr[0].equals("")) continue;
+			
+			int score = 0, seq = 0;
+			for(int j = 0; j < arr.length; j++) {
+				if(arr[j].equals("O")) {
+					seq++;
+					score += seq;
+				}
+				else {
+					seq = 0;
+				}
+			}
+			System.out.println(score);
 		}
 		
+		sc.close();
 	}
 }
