d0e6e6cccff9c38df5bb4c6f8ebd570d8613ca03
33cks <33cks@LAPTOP-QRDLLPVD>
2025-01-13 17:49:15 +0900

    Initial Commit

--- Backjoon/src/q1157/Main.java
+++ Backjoon/src/q1157/Main.java
@@ -10,20 +10,33 @@ public class Main {
 		String sen = sc.nextLine();
 		sc.close();
 		
-		sen = sen.toLowerCase();
+		sen = sen.toUpperCase();
 		String[] s = sen.split("");
 		Arrays.sort(s);
 		
-		int cnt = 0;
-		for(int i = 0; i < s.length; i++) {
-			for (int j = 0; j < i; j++) {
-				if(s[i].equals(s[j])) {
-					break;
-				}
-				else {
-					cnt++;
-				}
+		int cnt = 1;
+		for(int i = 0; i < s.length -1 ; i++) {
+			if(!s[i].equals(s[i+1])) cnt++;
+		}
+		
+		String[] w = new String[cnt];
+		w[0] = s[0];
+		int wc = 1;
+		for(int i = 0; i < s.length -1 ; i++) {
+			if(!s[i].equals(s[i+1])) w[wc++] = s[i+1];
+		}
+		
+		int[] col = new int[cnt];
+		int ccnt = 0;
+		for(int i = 0; i < w.length; i++) {
+			for(int j = 0; j < s.length; j++) {
+				if(w[i] == s[j]) col[ccnt] = ccnt++;
 			}
+		}
+		
+		int max = 0;
+		for(int i = 0; i < col.length; i++) {
+			if(col[i] > max) max = col[i];
 		}
 	}
 }
