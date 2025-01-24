b5de1ce3e9c212518cbca63a38d3ee2ce3a92ece
33cks <33cks@LAPTOP-QRDLLPVD>
2025-01-24 14:05:22 +0900

    Commit

--- Backjoon/src/q3052/Main.java
+++ Backjoon/src/q3052/Main.java
@@ -20,15 +20,13 @@ public class Main {
 		for(int i = 0; i < rem.length; i++) { 
 			for(int j = 0; j < rem.length; j++) {
 				if(i <= j) continue;
-				
-				if(rem[i] == rem[j]) rem[j] = -1;
+				if(rem[i] == rem[j]) {
+					rem[j] = -1;
+					cnt++;
+				}
 			} 
 		}
 		
-		for(int i = 0; i < rem.length; i++) {
-			if(rem[i] != -1) cnt++;
-		}
-		
-		System.out.println(cnt);
+		System.out.println(10- cnt);
 	}
 }
